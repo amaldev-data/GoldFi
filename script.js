26c9b4229c79e481025972a3e521479f22fd7e38
Amaldev K M <amald0014@gmail.com>
2026-05-29 19:21:49 +0530

    Update API endpoint to production URL

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch('http://localhost:8000/predict', {
+            const response = await fetch('https://goldfi-swb8.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
